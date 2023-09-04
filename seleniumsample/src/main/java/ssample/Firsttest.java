0d0233c3d15134addb99c1bb858a93dab2ef9516
Prashanth-1946 <139128508+Prashanth-1946@users.noreply.github.com>
2023-09-04 09:53:28 +0530

    SampleTest

---
+++ seleniumsample/src/main/java/ssample/Firsttest.java
@@ -0,0 +1,13 @@
+package ssample;
+
+import org.testng.annotations.Test;
+
+public class Firsttest 
+{
+	@Test
+	public void test1() 
+	{
+		System.out.println("FirstTest-test1");
+	}
+
+}
---
+++ seleniumsample/src/main/java/ssample/SecondTest.java
@@ -0,0 +1,14 @@
+package ssample;
+
+import org.testng.annotations.Test;
+
+public class SecondTest 
+{
+	@Test
+	public void test2() 
+	{
+		System.out.println("SecondTest-test1");
+	}
+
+
+}
